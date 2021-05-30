51c5ee314da44b07dc3afe6138a7c5d7b1454a23
Ardo Dlamini <ardodlamini@gmail.com>
2021-05-30 23:12:25 +0200

    Optimal policy implementation complete.

--- ValueIteration.py
+++ ValueIteration.py
@@ -1,5 +1,8 @@
 import sys
 import pprint
+import matplotlib.pyplot as plt
+from Animate import generateAnimat
+import copy
 
 def main():
 	print("Welcome")
@@ -36,7 +39,7 @@ def main():
 				g = float(sys.argv[i+1]) 
 				
 				
-	envment=[[0]*width for _ in range (height)]
+	envment=[[0]*width for _ in range (height)] #(0,0) is the top left corner.
 	rewards=[[[0 for _ in range(4)] for _ in range(width)] for _ in range(height)] # 3D Array, All set to 0.
 	#element [..][..][0] stands for UP
 	#element [..][..][1] stands for LEFT
@@ -68,6 +71,8 @@ def main():
 	remaining_coords = 1 #Counts how many coordinates are left to be checked for this iteration.
 	nexti_remaining_coords = 0
 	
+	record = []
+	
 	while(len(queue)!=0): #When the queue is empty, convergence has been reached.
 		
 		current_coord = queue.pop(0)
@@ -77,32 +82,30 @@ def main():
 		
 		#Check if it's a terminal state, just add it's neighbours if it is a terminal state.
 		if(envment[current_coord[0]][current_coord[1]] != 100):
-			print('true')
-			#For now, set it to a 100.
 			results = []
 			
 			Reward = 0
 			Value = 0
 			
-			if(validcoord([current_coord[0],current_coord[1]+1], width, height)): #down
+			if(validcoord([current_coord[0],current_coord[1]+1], width, height)): #right
 					Reward = rewards[current_coord[0]][current_coord[1]+1][3]
 					Value = envment[current_coord[0]][current_coord[1]+1]
 					
 					results.append(action_value(Reward,g,Value))
 					
-			if(validcoord([current_coord[0],current_coord[1]-1], width, height)): #up
+			if(validcoord([current_coord[0],current_coord[1]-1], width, height)): #left
 					Reward = rewards[current_coord[0]][current_coord[1]-1][0]
 					Value = envment[current_coord[0]][current_coord[1]-1]
 					
 					results.append(action_value(Reward,g,Value))
 					
-			if(validcoord([current_coord[0]-1,current_coord[1]], width, height)): #left
+			if(validcoord([current_coord[0]-1,current_coord[1]], width, height)): #up
 					Reward = rewards[current_coord[0]-1][current_coord[1]][1]
 					Value = envment[current_coord[0]-1][current_coord[1]]
 					
 					results.append(action_value(Reward,g,Value))
 					
-			if(validcoord([current_coord[0]+1,current_coord[1]], width, height)): #right
+			if(validcoord([current_coord[0]+1,current_coord[1]], width, height)): #down
 					Reward = rewards[current_coord[0]+1][current_coord[1]][2]
 					Value = envment[current_coord[0]+1][current_coord[1]]
 					
@@ -112,22 +115,22 @@ def main():
 					
 		if(prev_value != envment[current_coord[0]][current_coord[1]] or envment[current_coord[0]][current_coord[1]] == 100): #If the value of the current state changed or we have just started, the neighbours may have to be updated as well.
 		
-			if(validcoord([current_coord[0],current_coord[1]+1], width, height)): #down
+			if(validcoord([current_coord[0],current_coord[1]+1], width, height)): #right
 				if(envment[current_coord[0]][current_coord[1]+1] != 100):
 					queue.append([current_coord[0],current_coord[1]+1])
 					nexti_remaining_coords += 1
 					
-			if(validcoord([current_coord[0],current_coord[1]-1], width, height)): #up
+			if(validcoord([current_coord[0],current_coord[1]-1], width, height)): #left
 				if(envment[current_coord[0]][current_coord[1]-1] != 100):
 					queue.append([current_coord[0],current_coord[1]-1])
 					nexti_remaining_coords += 1
 					
-			if(validcoord([current_coord[0]-1,current_coord[1]], width, height)): #left
+			if(validcoord([current_coord[0]-1,current_coord[1]], width, height)): #up
 				if(envment[current_coord[0]-1][current_coord[1]] != 100):
 					queue.append([current_coord[0]-1,current_coord[1]])
 					nexti_remaining_coords += 1
 					
-			if(validcoord([current_coord[0]+1,current_coord[1]], width, height)): #right
+			if(validcoord([current_coord[0]+1,current_coord[1]], width, height)): #down
 				if(envment[current_coord[0]+1][current_coord[1]] != 100):
 					queue.append([current_coord[0]+1,current_coord[1]])
 					nexti_remaining_coords += 1
@@ -136,14 +139,74 @@ def main():
 			remaining_coords = nexti_remaining_coords
 			nexti_remaining_coords = 0
 			#Export environment states to records.
+			record.append([envment])
 	
+	#Get optimal policy.
+	
+	start_coord = [starty,startx]
+	opt_pol = []
+	
+	
+	while(True):
+	
+		print(start_coord)
+		opt_pol.append(start_coord)
+		
+		if(start_coord[0]== endy and start_coord[1] == endx):
+			break
+			
+		#print(envment[start_coord[0]][start_coord[1]+1],envment[start_coord[0]][start_coord[1]-1],envment[start_coord[0]-1][start_coord[1]],envment[start_coord[0]+1][start_coord[1]])
+		valuelist = []
+		
+		if(validcoord([start_coord[0],start_coord[1]+1], width, height)):
+			valuelist.append(envment[start_coord[0]][start_coord[1]+1])
+			
+		if(validcoord([start_coord[0],start_coord[1]-1], width, height)):
+			valuelist.append(envment[start_coord[0]][start_coord[1]-1])
+			
+		if(validcoord([start_coord[0]-1,start_coord[1]], width, height)):
+			valuelist.append(envment[start_coord[0]-1][start_coord[1]])
+			
+		if(validcoord([start_coord[0]+1,start_coord[1]], width, height)):
+			valuelist.append(envment[start_coord[0]+1][start_coord[1]])
+				
+		best = max(valuelist)
+		
+		if(validcoord([start_coord[0],start_coord[1]+1], width, height)):
+			if(best == envment[start_coord[0]][start_coord[1]+1]):
+				start_coord = [start_coord[0],start_coord[1]+1]
+				continue
+		
+		if(validcoord([start_coord[0],start_coord[1]-1], width, height)):
+			if(best == envment[start_coord[0]][start_coord[1]-1]):
+				start_coord = [start_coord[0],start_coord[1]-1]
+				continue
+		
+		if(validcoord([start_coord[0]-1,start_coord[1]], width, height)):
+			if(best == envment[start_coord[0]-1][start_coord[1]]):
+				start_coord = [start_coord[0]-1,start_coord[1]]
+				continue
+		
+		if(validcoord([start_coord[0]+1,start_coord[1]], width, height)):
+			if(best == envment[start_coord[0]+1][start_coord[1]]):
+				start_coord = [start_coord[0]+1,start_coord[1]]
+				continue
+		
+	print(opt_pol)
 	print(envment)
+	print(record)
+	
+	#Produce animation.
+	start_state = (starty, startx)
+	end_state = (endy, endx)
+	
+	mines = []
 
 def action_value(reward, discount, value):
 	return reward + (discount * value)
 			
 def validcoord(coord, width, height):
-	print(coord)
+	#print(coord)
 	if(coord[0]>height-1):
 		return False
 		
